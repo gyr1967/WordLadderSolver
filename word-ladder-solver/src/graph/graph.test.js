96e24cc33576fdaaa7cc7274bf11e9d36670bd57
George Yarr <2553638y@student.gla.ac.uk>
2023-05-31 19:19:52 +0100

    added some very simple tests that check some cases of the bfs

---
+++ word-ladder-solver/src/graph/graph.test.js
@@ -0,0 +1,25 @@
+import Graph from './Graph.js';
+
+
+test('BFS should find a path in a graph', () => {
+    let graph = new Graph(3);
+
+    graph.setVertex(0, 'cat');
+    graph.setVertex(1, 'bat');
+    graph.setVertex(2, 'bam');
+    
+    graph.getVertex(0).addToAdjList(1);
+    graph.getVertex(1).addToAdjList(2);
+
+    expect(graph.bfs(graph.getVertex(0), graph.getVertex(2))).toBeTruthy();
+});
+
+test('BFS should not find a path in a graph', () => {
+    let graph = new Graph(3);
+
+    graph.setVertex(0, 'cat');
+    graph.setVertex(1, 'dog');
+    graph.setVertex(2, 'hit');
+    
+    expect(graph.bfs(graph.getVertex(0), graph.getVertex(2))).toBeFalsy();
+});
